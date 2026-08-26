6f500a1027d3f14c0cb0970c0809feb4535d2857
Dhruv Singh <hello.dhruv05@gmail.com>
2026-08-26 13:08:47 +0530

    Time: 1 ms (100%), Space: 44.2 MB (40.94%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n = s.length();
+        int left = 0;
+        int ones = 0;
+
+        int minLen = Integer.MAX_VALUE;
+        String ans = "";
+
+        for (int right=0; right<n; right++) {
+            if (s.charAt(right) == '1') {
+                ones++;
+            }
+
+            while (ones==k) {
+                int len = right-left+1;
+                String curr = s.substring(left, right+1);
+
+                if (len < minLen) {
+                    minLen = len;
+                    ans = curr;
+                } else if (len == minLen && curr.compareTo(ans) < 0) {
+                    ans = curr;
+                }
+
+                if (s.charAt(left) == '1') {
+                    ones--;
+                }
+
+                left++;
+                
+            }
+        }
+
+        return ans;
+    }
+}
